de977b83f26643f5435e95a51676e7bb5af09f15
CRAZYFAN125 <88847481+CRAZYFAN125@users.noreply.github.com>
2021-08-28 19:39:37 +0200

    update PlayerGui.cs

---
+++ CRAZYTALE/Assets/Skrypty/Gra/Gracz/PlayerGui.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class PlayerGui:MonoBehaviour{
+    [Header("Pauza:")]
+    public GameObject Pauza;
+    private bool isPaused = false;
+    public KeyCode PauseKey = KeyCode.Escape;
+    
+    public void PauseGame(){
+        isPaused = !isPaused;
+        Pauza.SetActive(isPaused);
+    }
+    
+    void Update(){
+        #region PauzaKeyingCheck
+        if(isPaused = false){
+            PauseGame();
+        }
+        #endregion
+        
+    }
+}
